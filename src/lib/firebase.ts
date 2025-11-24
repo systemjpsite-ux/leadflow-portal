890ca92ff646cc3338962d067043b786e0cb1b6c
Site Systemjp <systemjpsite@gmail.com>
2025-11-24 14:21:02 +0000

    Continue.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCXeImhZWnul1sP7H_DFdAvn-9_eVQkZ8w",
   authDomain: "leadflow-portal-fc917.firebaseapp.com",
   projectId: "leadflow-portal-fc917",
-  storageBucket: "leadflow-portal-fc917.appspot.com",
+  storageBucket: "leadflow-portal-fc917.firebasestorage.app",
   messagingSenderId: "1089649840506",
   appId: "1:1089649840506:web:70cc6bdd977997500ec6c3",
 };
