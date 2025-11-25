2c947966f5547efac0ec283abf09067190b55f0b
systemjpsite-ux <systemjpsite@gmail.com>
2025-11-26 05:27:09 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  }
 };
 
 export default nextConfig;
